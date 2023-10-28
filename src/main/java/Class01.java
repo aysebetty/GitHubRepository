4ae6297cec7ff91f28bcf9d62e98660eeaf46e79
aysebetty <95138426+aysebetty@users.noreply.github.com>
2023-10-28 15:28:38 -0400

    versiyon ekledim

---
+++ src/main/java/Class01.java
@@ -0,0 +1,30 @@
+public class Class01 {
+
+
+    public static void main(String[] args) {
+
+        //versiyon 1
+
+        System.out.println("versiyon 1 kodlari ");
+
+
+    }
+
+
+}
+/*Q  : cikis
+clear : ekrani temizler
+git init :
+git add . : gecici alana staging area eklenir
+git commit -m"versiyon1"
+git status : son durumu gor
+ git log --oneline
+git show versiyon linki :
+git commit --amend -"versiyon yeni ismi"
+git restore  : working spacedeki  tek bir dosyayi geri alir
+git restore . : working alandaki tum dosyalari geri ceker
+git restore --staged : cq
+git restore --staged . :
+git checkout versionlink . : vrsiom switch yapar
+git switch : geri al yapar
+  */
